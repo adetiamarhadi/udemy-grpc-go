6aecf9946b503c7219110916654f27277df55876
Adetia Marhadi <adetia.marhadi@gmail.com>
2022-06-01 10:50:39 +0700

    create blog

---
+++ blog/client/create.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/adetiamarhadi/udemy-grpc-go/blog/proto"
+)
+
+func createBlog(c pb.BlogServiceClient) {
+
+	log.Println("Call CreateBlog function")
+
+	blog := &pb.Blog{
+		AuthorId: "Adet",
+		Title:    "Pemrograman Go - Pemula",
+		Content:  "Bahasa Pemrograman Go",
+	}
+
+	res, err := c.CreateBlog(context.Background(), blog)
+	if err != nil {
+		log.Fatalf("Unexpected error: %v\n", err)
+	}
+
+	log.Printf("Blog has been created: %s\n", res.Id)
+}
---
+++ blog/client/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/adetiamarhadi/udemy-grpc-go/blog/proto"
+)
+
+var addr string = "localhost:50055"
+
+func main() {
+
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("can't connect to %s: %v/n", addr, err)
+	}
+
+	defer conn.Close()
+
+	c := pb.NewBlogServiceClient(conn)
+
+	createBlog(c)
+}
